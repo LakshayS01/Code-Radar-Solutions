1596938cc7cac375cbdc751533230245de56552a
LakshayS01 <lakshaysaini3987@gmail.com>
2025-02-08 16:11:24 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -20,9 +20,9 @@ int main() {
     {
         printf("%d",a*b);
     }
-    else 
-    {
-        printf("%d",a/b);
-    }
+    // else 
+    // {
+    //     printf("%d",a/b);
+    // }
     return 0;
 }
