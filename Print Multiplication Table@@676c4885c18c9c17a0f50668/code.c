16b3ef83db95acc4cbea78c4002e2032c4da47a8
LakshayS01 <lakshaysaini3987@gmail.com>
2025-02-13 12:58:31 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -7,11 +7,10 @@ char* welcome() {
 int main() {
     int n,i;
     scanf("%d",&n);
-    for (i=1;i<=n;i++)
+    for (i=1;i<=10;i++)
     {
         printf("%d x %d = %d \n",n,i,n*i);
 
     }   
-    
     return 0;
 }
